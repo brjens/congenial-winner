026c24950727adf9b89a79b66e959ca0ea9d40ca
Brandon Jensen <brjensen121@gmail.com>
2024-04-11 15:55:50 -0400

    updated gradle for mysql

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("mysql:mysql-connector-java:8.0.28")
+	//implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
